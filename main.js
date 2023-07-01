b5c520bf4f92c3e0e8ee995854fcd671bb812d15
tedsmoke27 <cesar.arango@est.iudigital.edu.co>
2023-06-30 20:38:49 -0500

    mi primera todo list

---
+++ main.js
@@ -0,0 +1,39 @@
+const todos= JSON.parse(localStorage.getItem("todos")) || [];
+
+
+
+const render = ( ) => {
+const todolist = document.getElementById("todo-list");
+const todotemplate = todos.map( t => "<li>"+ t + "</li>" );
+    todolist.innerHTML = todotemplate.join("");
+const elementos = document.querySelectorAll("#todo-list li")
+    elementos.forEach ((elemento, i ) => {
+    elemento.addEventListener("click",() => {
+    elemento.parentNode.removeChild(elemento)
+    todos.splice(i, 1)
+    actualizaTodos (todos)
+    render()    
+    })
+
+
+})
+}
+
+const actualizaTodos = (todos) => {
+ const todoStrings = JSON.stringify(todos)
+ localStorage.setItem("todos", todoStrings)
+
+}
+window.onload = () => {
+render()
+const form = document.getElementById("todo-form");
+form .onsubmit = (e) => {
+e.preventDefault(); 
+const todo = document.getElementById("todo");
+const todotext = todo.value;
+todo.value = "";
+todos.push(todotext);
+actualizaTodos (todos)
+render ();
+}
+}
